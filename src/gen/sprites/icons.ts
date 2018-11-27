6385e2ea1faa7a4beb0bb184882e2e0a59c11abb
aceeu <ace.evg@gmail.com>
2018-11-27 22:54:45 +0300

    +layer control

--- src/gen/sprites/icons.ts
+++ src/gen/sprites/icons.ts
@@ -6,36 +6,40 @@ import { IconInfoMap, iconInfo as i } from '../../common/sprite';
 export const SPRITE_NAME = 'icons';
 
 export const enum Classes {
-  Cave_2 = 'i s0 x37 i0',
-  Country = 'i s0 x37 i1',
-  Fillingstation = 'i s0 x37 i2',
-  Forest2 = 'i s0 x37 i3',
-  Geyser_2 = 'i s0 x37 i4',
-  Landmark = 'i s0 x37 i5',
-  Lodging_2 = 'i s0 x37 i6',
-  Mountains = 'i s0 x37 i7',
-  NeBarn_2 = 'i s0 x37 i8',
-  Parkinggarage = 'i s0 x37 i9',
-  River_2 = 'i s0 x37 i10',
-  Smallcity = 'i s0 x37 i11',
-  Tree = 'i s0 x37 i12',
-  Water = 'i s0 x37 i13'
+  BridgeOld = 'i s0 x37 i0',
+  Cave_2 = 'i s0 x37 i1',
+  Country = 'i s0 x37 i2',
+  Fillingstation = 'i s0 x37 i3',
+  Forest2 = 'i s0 x37 i4',
+  Geyser_2 = 'i s0 x37 i5',
+  Landmark = 'i s0 x37 i6',
+  Lodging_2 = 'i s0 x37 i7',
+  Mountains = 'i s0 x37 i8',
+  NeBarn_2 = 'i s0 x37 i9',
+  Parkinggarage = 'i s0 x37 i10',
+  River_2 = 'i s0 x37 i11',
+  Smallcity = 'i s0 x37 i12',
+  Tree = 'i s0 x37 i13',
+  Water = 'i s0 x37 i14',
+  Waterwellpump = 'i s0 x37 i15'
 }
 
 // Information about the icons
 export const info: IconInfoMap = {
-  'cave-2': i('i s0 x37 i0', 37, 0, 37, 37),
-  'country': i('i s0 x37 i1', 37, 74, 37, 37),
-  'fillingstation': i('i s0 x37 i2', 0, 37, 37, 37),
-  'forest2': i('i s0 x37 i3', 37, 37, 37, 37),
-  'geyser-2': i('i s0 x37 i4', 74, 0, 37, 37),
-  'landmark': i('i s0 x37 i5', 74, 37, 37, 37),
-  'lodging-2': i('i s0 x37 i6', 0, 74, 37, 37),
-  'mountains': i('i s0 x37 i7', 0, 0, 37, 37),
-  'ne_barn-2': i('i s0 x37 i8', 74, 74, 37, 37),
-  'parkinggarage': i('i s0 x37 i9', 111, 0, 37, 37),
-  'river-2': i('i s0 x37 i10', 111, 37, 37, 37),
-  'smallcity': i('i s0 x37 i11', 111, 74, 37, 37),
-  'tree': i('i s0 x37 i12', 0, 111, 37, 37),
-  'water': i('i s0 x37 i13', 37, 111, 37, 37)
+  'bridge_old': i('i s0 x37 i0', 37, 0, 37, 37),
+  'cave-2': i('i s0 x37 i1', 74, 74, 37, 37),
+  'country': i('i s0 x37 i2', 0, 37, 37, 37),
+  'fillingstation': i('i s0 x37 i3', 37, 37, 37, 37),
+  'forest2': i('i s0 x37 i4', 74, 0, 37, 37),
+  'geyser-2': i('i s0 x37 i5', 74, 37, 37, 37),
+  'landmark': i('i s0 x37 i6', 0, 74, 37, 37),
+  'lodging-2': i('i s0 x37 i7', 37, 74, 37, 37),
+  'mountains': i('i s0 x37 i8', 0, 0, 37, 37),
+  'ne_barn-2': i('i s0 x37 i9', 111, 0, 37, 37),
+  'parkinggarage': i('i s0 x37 i10', 111, 37, 37, 37),
+  'river-2': i('i s0 x37 i11', 111, 74, 37, 37),
+  'smallcity': i('i s0 x37 i12', 0, 111, 37, 37),
+  'tree': i('i s0 x37 i13', 37, 111, 37, 37),
+  'water': i('i s0 x37 i14', 74, 111, 37, 37),
+  'waterwellpump': i('i s0 x37 i15', 111, 111, 37, 37)
 };
--- src/leaflet.js
+++ src/leaflet.js
@@ -7,29 +7,37 @@ import './plugins/Leaflet.PolylineMeasure.css';
 let pedia = null;
 
 export function LeafletBaseMap(element, startPos) {
-    var mymap = L.map(element).setView(startPos, 9);
 
-    L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    const mapbox = L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
         attribution: 'aceeu',
         maxZoom: 18,
         id: 'mapbox.streets',
         accessToken: 'pk.eyJ1IjoiYWNlZXUiLCJhIjoiY2pvZzR6OXg3MDJuMDN3bnhmb24zcGt5biJ9.zpuG05zrtlhpz7LVLVmxcg'
-    }).addTo(mymap);
-    // L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    //     maxZoom: 16,
-    //     attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-    // }).addTo(mymap);
+    });
+    const osm = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+        maxZoom: 18,
+        attribution: 'Map data: &copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+    });
+
+    const baseMaps = {
+        "Mapbox": mapbox,
+        "OSM": osm
+    };
+
+    const overlayMaps = {
+        // "Cities": cities
+    };
+
+    let mymap = L.map(element, {
+        center: startPos,
+        zoom: 7,
+        layers: [mapbox]
+    });
+    L.control.layers(baseMaps, overlayMaps).addTo(mymap);
+
 
     L.control.mousePosition().addTo(mymap);
     L.control.polylineMeasure().addTo(mymap);
-
-    mymap.on('zoom', () => {
-        const z = mymap.getZoom();
-        if (z > 10 && pedia == null)
-            {}
-        if (z <= 10 && pedia != null) {
-        }
-    });
     return mymap;
 }
 
