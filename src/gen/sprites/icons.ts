4d8ef7957f8317895cc979233a0de6a6fcbc328a
aceeu <ace.evg@gmail.com>
2024-04-07 09:47:22 +0300

    добавил иконку food

--- src/gen/sprites/icons.ts
+++ src/gen/sprites/icons.ts
@@ -13,15 +13,16 @@ export const enum Classes {
   Cheb = 'i s0 x56 i4',
   FlagOrange = 'i s0 x32 i5',
   Flag = 'i s0 x32 i6',
-  Geyser = 'i s0 x56 i7',
-  Information = 'i s0 x56 i8',
-  Lake = 'i s0 x56 i9',
-  Mount = 'i s0 x56 i10',
-  RoadSand = 'i s0 x56 i11',
-  See = 'i s0 x56 i12',
-  Town = 'i s0 x56 i13',
-  Tree = 'i s0 x56 i14',
-  Vulkan = 'i s0 x56 i15'
+  Food = 'i s0 x54 i7',
+  Geyser = 'i s0 x56 i8',
+  Information = 'i s0 x56 i9',
+  Lake = 'i s0 x56 i10',
+  Mount = 'i s0 x56 i11',
+  RoadSand = 'i s0 x56 i12',
+  See = 'i s0 x56 i13',
+  Town = 'i s0 x56 i14',
+  Tree = 'i s0 x56 i15',
+  Vulkan = 'i s0 x56 i16'
 }
 
 // Information about the icons
@@ -31,15 +32,16 @@ export const info: IconInfoMap = {
   'canyon': i('i s0 x56 i2', 0, 56, 56, 56),
   'cave': i('i s0 x56 i3', 56, 56, 56, 56),
   'cheb': i('i s0 x56 i4', 112, 0, 56, 56),
-  'flag-orange': i('i s0 x32 i5', 112, 168, 32, 32),
-  'flag': i('i s0 x32 i6', 144, 168, 32, 32),
-  'geyser': i('i s0 x56 i7', 112, 56, 56, 56),
-  'information': i('i s0 x56 i8', 0, 112, 56, 56),
-  'lake': i('i s0 x56 i9', 56, 112, 56, 56),
-  'mount': i('i s0 x56 i10', 112, 112, 56, 56),
-  'road_sand': i('i s0 x56 i11', 168, 0, 56, 56),
-  'see': i('i s0 x56 i12', 168, 56, 56, 56),
-  'town': i('i s0 x56 i13', 168, 112, 56, 56),
-  'tree': i('i s0 x56 i14', 0, 168, 56, 56),
-  'vulkan': i('i s0 x56 i15', 56, 168, 56, 56)
+  'flag-orange': i('i s0 x32 i5', 166, 168, 32, 32),
+  'flag': i('i s0 x32 i6', 224, 0, 32, 32),
+  'food': i('i s0 x54 i7', 112, 168, 54, 54),
+  'geyser': i('i s0 x56 i8', 112, 56, 56, 56),
+  'information': i('i s0 x56 i9', 0, 112, 56, 56),
+  'lake': i('i s0 x56 i10', 56, 112, 56, 56),
+  'mount': i('i s0 x56 i11', 112, 112, 56, 56),
+  'road_sand': i('i s0 x56 i12', 168, 0, 56, 56),
+  'see': i('i s0 x56 i13', 168, 56, 56, 56),
+  'town': i('i s0 x56 i14', 168, 112, 56, 56),
+  'tree': i('i s0 x56 i15', 0, 168, 56, 56),
+  'vulkan': i('i s0 x56 i16', 56, 168, 56, 56)
 };
